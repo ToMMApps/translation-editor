435a5c3a1522fcb01811eed4d24cf05fcea79a33
henning96 <ger.henning96@gmail.com>
2015-08-27 13:05:49 +0200

    done init

---
+++ index.js
@@ -0,0 +1,194 @@
+/**
+ * Small library to operate on translation objects like those which are part of angular-translate.
+ * This library is meant to be used on both client and server-side.
+ * @author Henning Gerrits
+ * @type {*|exports|module.exports}
+ */
+
+var Q = require('q');
+var util = require('util');
+var isObjectEmpty = require('is-object-empty');
+
+var api = Object.create(null);
+
+function splitKey(key){
+    return key.split('.');
+}
+
+/**
+ * Add key to the translation object. Non-existing objects will be created.
+ * The overhanded object will be modified.
+ * @param {string} key
+ * @param {string} value
+ * @param {object} translationObject
+ * @returns {promise}
+ */
+api.addKey = function(key, value, translationObject){
+    if(util.isNullOrUndefined(translationObject)){
+        return Q.reject(new TypeError('translationObject is not an object'));
+    }
+
+    var splitted = splitKey(key);
+    var obj = translationObject;
+    var length = splitted.length;
+    var prop;
+
+    for(var i = 0; i < length -1; i++){
+        prop = obj[splitted[i]];
+
+        if(util.isNullOrUndefined(prop)){
+            obj[splitted[i]] = Object.create(null);
+        } else if(util.isString(prop)){
+            return Q.reject(new Error("element would be overwritten"));
+        } else if(util.isObject(prop)){
+            /* ignore */
+        } else {
+            return Q.reject(new TypeError('unexpected type'));
+        }
+
+        obj = obj[splitted[i]];
+    }
+
+    prop = obj[splitted[length-1]];
+
+    if(util.isNullOrUndefined(prop)){
+        obj[splitted[length-1]] = value;
+    } else if(util.isString(prop)){
+        return Q.reject(new Error("element would be overwritten"));
+    } else if(util.isObject(prop)){
+        return Q.reject(new Error("element would be overwritten"));
+    } else {
+        return Q.reject(new TypeError('unexpected type'));
+    }
+
+    return Q(translationObject);
+};
+
+/**
+ *
+ * @param {string} key
+ * @param {object} translationObject
+ * @returns {promise}
+ */
+function search(key, translationObject){
+    var splitted = splitKey(key);
+    var obj = translationObject;
+    var length = splitted.length;
+    var prop;
+    var history = [translationObject];
+
+    for(var i = 0; i < length -1; i++){
+        prop = obj[splitted[i]];
+
+        if(util.isNullOrUndefined(prop)){
+            return Q();
+        } else if(util.isString(prop)){
+            return Q();
+        } else if(util.isObject(prop)){
+            /* ignore */
+        } else {
+            return Q.reject(new TypeError('unexpected type'));
+        }
+
+        obj = prop;
+        history.push(prop);
+    }
+
+    prop = obj[splitted[length-1]];
+
+    if(util.isNullOrUndefined(prop)){
+        return Q();
+    } else if(util.isString(prop)){
+        return Q({translation: obj[splitted[length-1]], history: history, propName: splitted[length-1]});
+    } else if(util.isObject(prop)){
+        return Q();
+    } else {
+        return Q.reject(new TypeError('unexpected type'));
+    }
+}
+
+/**
+ * Removes the given key from the translation object. Objects that become empty due to to this
+ * operation will be deleted, too.
+ * @param {string} key
+ * @param {object} translationObject
+ * @returns {promise}
+ */
+api.delKey = function(key, translationObject){
+    return search(key, translationObject)
+        .then(function(result){
+
+            if(util.isNullOrUndefined(result)){
+                return Q.reject('element does not exist');
+            } else {
+                var history = result.history;
+                delete history[history.length-1][result.propName];
+
+                for(var i = history.length-1; i >= 0; i--){
+                    var obj = history[i];
+
+                    var emptyObjects = Object.keys(obj).filter(function(key){
+                        return util.isObject(obj[key]) && isObjectEmpty(obj[key]);
+                    });
+
+                    emptyObjects.forEach(function(key){
+                        delete obj[key];
+                    });
+                }
+
+                return Q(translationObject);
+            }
+        });
+};
+
+/**
+ * Returns the associated value or undefined if the given key was not found.
+ * @param {string} key
+ * @param {object} translationObject
+ * @returns {promise}
+ */
+api.getValue = function(key, translationObject){
+    return search(key, translationObject)
+        .then(function(result){
+            return Q(result? result.translation : undefined);
+        });
+};
+
+/**
+ * Returns true if the given key exists in the translation object.
+ * @param {string} key
+ * @param {object} translationObject
+ * @returns {promise}
+ */
+api.keyExists = function(key, translationObject){
+    return api.getValue(key, translationObject)
+        .then(function(translation){
+            return Q(!util.isNullOrUndefined(translation));
+        });
+};
+
+/**
+ * Changes the key. The corresponding value will be copied.
+ * @param {string} oldKey
+ * @param {string} newKey
+ * @param {object} translationObject
+ * @returns {promise}
+ */
+api.changeKey = function(oldKey, newKey, translationObject){
+    return api.keyExists(newKey, translationObject)
+        .then(function(exists){
+            if(exists){
+                return Q.reject(new Error("newKey does already exist"));
+            } else {
+                return api.getValue(oldKey, translationObject)
+                    .then(function(value){
+                        return api.delKey(oldKey, translationObject)
+                            .then(function(){
+                                return api.addKey(newKey, value, translationObject);
+                            });
+                    });
+            }
+        });
+};
+
+module.exports = api;
